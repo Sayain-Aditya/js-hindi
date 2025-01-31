5c6b125dcd4e641949b579c04fa6fab79af29fb7
Sayain-Aditya <as4316664@gmail.com>
2025-01-31 18:13:05 +0530

    n

---
+++ 01_basic/variables.js
@@ -0,0 +1,14 @@
+const accountid = 14453
+let accountEmail = "as431@gmail.com"
+var accountPassword = "1223"
+accountCity = "jaipur"
+let accounState;
+var account;            
+
+// accountid = 2  //not allowed
+accountEmail = "hc@h.com"
+accountPassword = "2323"
+accountCity = "delhi"
+// console.log(accountid);
+
+console.table([accountEmail, accountPassword, accountCity, accounState, account])
